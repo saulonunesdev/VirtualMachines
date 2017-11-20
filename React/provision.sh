5edfc87514f18364f0150db285e3960c9b0c2138
Saulo Nunes <saulo@sidsistemas.com>
2017-11-20 10:49:13 -0200

    Add SH command upgrade

--- React/provision.sh
+++ React/provision.sh
@@ -6,3 +6,5 @@ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 echo "Installing Node"
 sudo apt-get install -y nodejs
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+echo "Upgrading"
+sudo apt-get -y upgrade
