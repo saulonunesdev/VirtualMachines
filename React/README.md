09881b6523da1dfb3b4f3dde67a6595b9dad61cd
Saulo Nunes <saulo@sidsistemas.com>
2017-11-20 17:12:44 -0200

    Symlinks Bug Fix

--- React/README.md
+++ React/README.md
@@ -5,5 +5,16 @@ A React Virtual Machine with Vagrant and VirtualBox
 ### Commands
 
 ```
-npm install --no-bin-links
+Run vagrant up as Administrator (Windows Users)
+go to /var/www/projects/
+create your / git clone your project
+Run: sudo ln -s /home/vagrant/node_modules/ node_modules
+npm install
 ```
+
+
+### NPM Error Access Denied
+
+...
+npm config set unsafe-perm=true
+...
--- React/provision.sh
+++ React/provision.sh
@@ -1,10 +1,14 @@
 echo "Update Linux"
 sudo apt-get update
 echo "Installing Dependencies"
+sudo apt-get install git -y
+sudo apt-get install -y g++
 sudo apt-get install -y build-essential
-curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 echo "Installing Node"
 sudo apt-get install -y nodejs
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-echo "Upgrading"
-sudo apt-get -y upgrade
+echo "Creating Node Modules"
+mkdir /home/vagrant/node_modules
+echo "Permissions to Nod Modules"
+sudo chmod -R ugo+rw /home/vagrant/
