2c51251facb4f0049fe5ef508072f0db1f7559f3
Saulo Nunes <saulo@paralink.com.br>
2017-12-07 10:19:24 -0200

    Installing SDK

--- React/app/firstrun.sh
+++ React/app/firstrun.sh
@@ -1,41 +1,42 @@
+echo "Installing JAVA8"
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
 sudo apt-get install oracle-java8-installer -y
-echo JAVA_HOME="/usr/lib/jvm/java-8-oracle" >> /etc/environment
-source /etc/environment
-echo export JAVA_HOME="/usr/lib/jvm/java-8-oracle" >> ~/.bashrc
-echo export PATH=$JAVA_HOME/bin:$PATH >> ~/.bashrc
-source /etc/profile
+sudo echo export JAVA_HOME="/usr/lib/jvm/java-8-oracle" >> ~/.bashrc
+source ~/.bashrc
+sudo echo export PATH=$JAVA_HOME/bin:$PATH >> ~/.bashrc
+source ~/.bashrc
 
-sudo apt-get install -y unzip make
+echo "installing Android Studio"
 cd ~
+sudo apt-get install libc6-dev-i386 lib32z1 ia32-libs -y
 wget https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
 tar xf android-sdk*-linux.tgz
-wget https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
-unzip android-ndk*.zip
 cd android-sdk-linux/tools
 sudo ./android update sdk --no-ui
-sudo ./android update sdk --all --no-ui --filter $(seq -s, 27)
-
-echo 'export ANDROID_HOME=$HOME/android-sdk-linux' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools' >> ~/.bashrc
-
+cd ~
+sudo echo export ANDROID_HOME=$HOME/android-sdk-linux >> ~/.bashrc
 source ~/.bashrc
+sudo echo export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/27.0.1:$ANDROID_HOME/tools/bin >> ~/.bashrc
+source ~/.bashrc
+
+sudo touch ~/.android/repositories.cfg
 
 sudo update-ca-certificates -f
 
 echo SUBSYSTEM=="usb", ATTR{idVendor}=="22b8", MODE="0666", GROUP="plugdev" | sudo tee /etc/udev/rules.d/51-android-usb.rules
 
+
+#sudo apt-get install android-tools-adb
 sudo adb kill-server
 sudo adb start-server
-adb devices
+sudo adb devices
 
-cd /var/www/projects/
 
-git clone https://saulonunes@github.com/crosoften/kolabe.git
-cd kolabe/
-sudo ln -s /home/vagrant/node_modules/ node_modules
+#cd /var/www/projects/
+#git clone project
+#cd project/
+#sudo ln -s /home/vagrant/node_modules/ node_modules
 echo sdk.dir = /home/vagrant/android-sdk-linux > android/local.properties
-npm i -g react-native-cli
 npm install
 #sudo react-native run-android
--- React/provision.sh
+++ React/provision.sh
@@ -5,19 +5,32 @@ echo "Installing Git"
 sudo apt-get install git -y
 
 echo "Installing Dependencies"
+sudo apt-get install -y unzip make
 sudo apt-get install -y g++
 sudo apt-get install -y build-essential
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 echo "Installing Node"
 sudo apt-get install -y nodejs
+#sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+#echo "Creating Node Modules"
+#mkdir /home/vagrant/node_modules
+
+#echo "Permissions to Nod Modules"
+sudo chmod -R ugo+rw /home/vagrant/
+
+echo "Installing WatchMan"
+sudo apt-get install python-dev -y
+sudo apt-get install autoconf autogen -y
+git clone https://github.com/facebook/watchman.git
+cd watchman
+git checkout v4.1.0
+./autogen.sh
+./configure
+make
+sudo make install
 
 echo "Installing React Native Cli"
-sudo npm i -g react-native-cli
-
-echo "Creating Node Modules"
-mkdir /home/vagrant/node_modules
-
-echo "Permissions to Nod Modules"
-sudo chmod -R ugo+rw /home/vagrant/
+sudo npm install -g react-native-cli
+sudo npm install -g flow-bin
