61f2c1c523a75141ef3f42717770924817b63366
Saulo Nunes <saulo@paralink.com.br>
2018-04-16 20:49:33 -0300

    Container Vagrant LAMP

---
+++ Lamp-Container/backup/bootstrap.sh
@@ -0,0 +1,10 @@
+
+sudo apt-get update
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y vim curl python-software-properties
+sudo apt-get update
+sudo apt-get -y install mysql-server
+sed -i "s/^bind-address/#bind-address/" /etc/mysql/my.cnf
+mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
+sudo /etc/init.d/mysql restart
