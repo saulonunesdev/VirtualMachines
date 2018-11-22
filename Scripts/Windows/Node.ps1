3909b572a31f3bac31919148a834de1b63b6f27f
SauloNunes <saulo@paralink.com.br>
2018-11-22 13:09:31 -0200

    Ajustes nos scripts de isntalacao do NODE

--- Scripts/Windows/Node.ps1
+++ Scripts/Windows/Node.ps1
@@ -1,13 +1,16 @@
 # Installation Node
 choco install -y nodejs.install
-#choco install -y jdk8
-
-#Packages
-choco install yarn -y
-RefreshEnv
+choco install -y python2
+choco install -y yarn
+choco install -y microsoft-build-tools-2013
+#Packages And Configuration
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 npm install --global --production windows-build-tools
 npm config set msvs_version 2015 --global
-npm install -g gulp-cli
-npm install -g bower
-npm install -g grunt-cli
+npm config set python python2.7
 npm install -g http-server
+npm install -g gulp-cli
+npm install -g electron
+
+#https://www.microsoft.com/en-us/download/details.aspx?id=40760 Microsoft Build Tools 2013
+#https://www.microsoft.com/en-us/download/confirmation.aspx?id=48159
