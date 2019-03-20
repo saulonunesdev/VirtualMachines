894136e5a75743147c3ed087678c6c780952acf7
SauloNunes <saulo@paralink.com.br>
2019-03-19 22:41:25 -0300

    DelphiCE Moved to a self repo, removed electron vm

--- ELECTRONWIN/FirstRun.ps1
+++
@@ -1,4 +0,0 @@
-#Run These Commands Manually in Windows PowerShell Admin Mode
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco upgrade chocolatey
-choco -v
